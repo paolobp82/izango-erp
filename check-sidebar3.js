f4319a065fad2eb71896d6845f92b0f1a643e539
paolobp82 <paolobp@gmail.com>
2026-04-24 23:28:35 -0500

    chore: limpieza de scripts temporales

--- check-sidebar3.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("components/layout/Sidebar.tsx", "utf8");
-const idx = c.indexOf("return (");
-console.log(JSON.stringify(c.substring(idx, idx+600)));
--- check-sidebar4.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const c = fs.readFileSync("components/layout/Sidebar.tsx", "utf8");
-const idx = c.indexOf("borderBottom");
-console.log(JSON.stringify(c.substring(idx-50, idx+400)));
